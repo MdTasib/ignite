a33f79f3f77f6f6b3727ff71282ed62d3d0c2d91
MdTasib <ohidulalam442@gmail.com>
2022-08-21 21:32:25 +0600

    Setting Up Redux

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import gamesReducer from "./gamesReducers";
+
+const rootReducers = combineReducers({
+	games: gamesReducer,
+});
+
+export default rootReducers;
