a2a1ec8a10750766160e1ff8e89d7a2bb9fc6c98
MdTasib <ohidulalam442@gmail.com>
2022-08-22 12:50:38 +0600

    10.Fetch Game Detail

--- src/api.js
+++ src/api.js
@@ -4,8 +4,6 @@ const key = "5cd47b7278d94d7e83a19d3b5e7cc8d0";
 // BASE URL
 const base_url = "https://api.rawg.io/api/";
 
-// https://api.rawg.io/api/games?key=5cd47b7278d94d7e83a19d3b5e7cc8d0
-
 // GETTING THE MONTH
 const getCurrentMonth = () => {
 	const month = new Date().getMonth() + 1;
@@ -35,6 +33,8 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // POPULAR GAMES
+// https://api.rawg.io/api/games?key=5cd47b7278d94d7e83a19d3b5e7cc8d0
+
 const popular_games = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 const upcoming_games = `games?key=${key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 const newGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
@@ -43,4 +43,25 @@ const popularGamesURL = () => `${base_url}${popular_games}`;
 const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 const newGamesURL = () => `${base_url}${newGames}`;
 
-export { popularGamesURL, upcomingGamesURL, newGamesURL };
+// GAME DETAILS
+// https://api.rawg.io/api/games?key=5cd47b7278d94d7e83a19d3b5e7cc8d0&668024
+// https://api.rawg.io/api/games?key=5cd47b7278d94d7e83a19d3b5e7cc8d0&668024/screenshots
+
+const gameDetailsURL = game_id => `${base_url}games?key=${key}&${game_id}`;
+
+// GAME SCREENSHOTS
+const gameScreenshotURL = game_id =>
+	`${base_url}games?key=${key}&${game_id}/screenshots`;
+
+// SEARCHED GAME
+const searchGameURL = game_name =>
+	`${base_url}games?search=${game_name}&page_size=9`;
+
+export {
+	popularGamesURL,
+	upcomingGamesURL,
+	newGamesURL,
+	gameDetailsURL,
+	gameScreenshotURL,
+	searchGameURL,
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import gamesReducer from "./gamesReducers";
+import detailReducer from "./detailReducer";
 
 const rootReducers = combineReducers({
 	games: gamesReducer,
+	detail: detailReducer,
 });
 
 export default rootReducers;
